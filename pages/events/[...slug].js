8e35ad15d747c0e8bad0d198873dca27758214a4
nqdai <nguyendai1992@gmail.com>
2021-05-11 23:00:11 +0700

    Setup router

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function FilterdEventsPage () {
+    return (
+        <div>
+            <h1>Filterd Event Page</h1>
+        </div>
+    )
+}
+
+export default FilterdEventsPage
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Link from 'next/link'
 function HomePage () {
   return (
     <div>
       <h1>The Home Page</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-      </ul>
     </div>
   )
 }
